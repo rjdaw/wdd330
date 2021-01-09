0624c9cbe92d406530fa16dad04d6c42b9b96c83
rjdaw <daw19003@byui.edu>
2021-01-09 13:45:42 -0700

    creating input form

--- scripts/main.js
+++ scripts/main.js
@@ -13,3 +13,27 @@ links.forEach (
         `<li><a href="${link.url}">${link.label}</a></li>`;
     }
 )
+
+//Display local storage
+const inputKey = document.getElementById("inputKey");
+const inputValue = document.getElementById("inputValue");
+const insert = document.getElementById("insert");
+const lsOutput = document.getElementsByClassName("lsOutput");
+
+insert.onclick = function () {
+  const key = inputKey.value;
+  const value = inputValue.value;
+
+  if (key && value) {
+    localStorage.setItem(key, value);
+    location.reload();
+  }
+
+  for (let i = 0; i < localStorage.length; i++) {
+    const key = localStorage.key(i);
+    const value = localStorage.getItem(key);
+
+    lsOutput.innerHTML += `${key}: ${value}<br>`;
+  }
+
+}
