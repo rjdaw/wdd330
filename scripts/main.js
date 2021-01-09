e0d49d739cfc2452bae530ac1c938b6be31d5e80
rjdaw <daw19003@byui.edu>
2021-01-09 14:32:55 -0700

    correcting js

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,10 @@
-let ol = document.querySelector('ol');
-
-const links = [
-    {
-      label: "Week1 Notes",
-      url: "week1/notes.html"
-    }
-  ];
-
-links.forEach (
-    link => {
-        ol.innerHTML += 
-        `<li><a href="${link.url}">${link.label}</a></li>`;
-    }
-)
-
 //Display local storage
 const inputKey   = document.getElementById("inputKey");
 const inputValue = document.getElementById("inputValue");
-const insert     = document.getElementById("insert");
+const btnInsert     = document.getElementById("btnInsert");
 const lsOutput   = document.getElementById("lsOutput");
 
-insert.onclick = function () {
+btnInsert.onclick = function () {
   const key = inputKey.value;
   const value = inputValue.value;
 
