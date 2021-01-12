f57cc4a06f2b15c19039fc0fb57ad34b382f0a19
rjdaw <daw19003@byui.edu>
2021-01-12 10:01:40 -0700

    recreating main js

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,15 @@
-//Display local storage
-const inputKey   = document.getElementById("inputKey");
-const inputValue = document.getElementById("inputValue");
-const btnInsert     = document.getElementById("btnInsert");
-const lsOutput   = document.getElementById("lsOutput");
+let ol = document.querySelector('ol');
 
-btnInsert.onclick = function () {
-  const key = inputKey.value;
-  const value = inputValue.value;
+const links = [
+    {
+        label: "Week 1 Notes",
+        url: "week1/notes.html"
+    }
+];
 
-  if (key && value) {
-    localStorage.setItem(key, value);
-    location.reload();
-  }
-};
-
-for (let i = 0; i < localStorage.length; i++) {
-  const key = localStorage.key(i);
-  const value = localStorage.getItem(key);
-
-  lsOutput.innerHTML += `${key}: ${value}<br>`;
-}
+links.forEach(
+    link => {
+        ol.innerHTML +=
+        `<li><a href="${link.url}">${link.label}</a></li>`;
+    }
+);
---
+++ scripts/week1.js
@@ -0,0 +1,22 @@
+//Display local storage
+const inputKey   = document.getElementById("inputKey");
+const inputValue = document.getElementById("inputValue");
+const btnInsert     = document.getElementById("btnInsert");
+const lsOutput   = document.getElementById("lsOutput");
+
+btnInsert.onclick = function () {
+  const key = inputKey.value;
+  const value = inputValue.value;
+
+  if (key && value) {
+    localStorage.setItem(key, value);
+    location.reload();
+  }
+};
+
+for (let i = 0; i < localStorage.length; i++) {
+  const key = localStorage.key(i);
+  const value = localStorage.getItem(key);
+
+  lsOutput.innerHTML += `${key}: ${value}<br>`;
+}
