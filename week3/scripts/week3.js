98864dbc2f1ddf20631bdc0a593fe043981f73dd
rjdaw <daw19003@byui.edu>
2021-01-19 15:19:58 -0700

    creating student object

--- week3/scripts/week3.js
+++ week3/scripts/week3.js
@@ -1,5 +1,10 @@
-const firstName = 'Ruben';
-const lastName = 'Dawagne';
+// const firstName = 'Ruben';
+// const lastName = 'Dawagne';
+
+const student = {
+    firstName: 'Ruben',
+    lastName: 'Dawagne'
+};
 
 const section1 = document.querySelector('.js-testing');
 
@@ -7,11 +12,11 @@ const div1 = document.createElement('div');
 div1.setAttribute('id', 'div1');
 div1.classList.add('emphasize');
 
-div1.textContent = firstName;
+div1.textContent = student.firstName;
 
 section1.appendChild(div1);
 
 const div2 = document.createElement('div');
-div2.textContent = lastName;
+div2.textContent = student.lastName;
 
 section1.appendChild(div2);
