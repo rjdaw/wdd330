d713bd389373584dae795423b7e30a6ba774a61b
rjdaw <daw19003@byui.edu>
2021-01-19 15:54:29 -0700

    updating dynamic js file

--- week3/scripts/week3.js
+++ week3/scripts/week3.js
@@ -3,7 +3,18 @@
 
 const student = {
     firstName: 'Ruben',
-    lastName: 'Dawagne'
+    lastName: 'Dawagne',
+    age: 26,
+    iNumber: '255950416',
+    hobbies: [
+        'soccer',
+        'board games',
+        'programming',
+    ],
+    favoriteBook: {
+        title: "La Panthere Noire",
+        author: "Regis Montulet"
+    }
 };
 
 const section1 = document.querySelector('.js-testing');
@@ -20,3 +31,25 @@ const div2 = document.createElement('div');
 div2.textContent = student.lastName;
 
 section1.appendChild(div2);
+
+const div3 = document.createElement('div');
+div3.textContent = student.favoriteBook.author;
+
+section1.appendChild(div3);
+
+const div4 = document.createElement('div');
+// div4.textContent = student.hobbies.join(', ');
+
+const ul = document.createElement('ul');
+student.hobbies.forEach(
+    hobby => {
+        const li = document.createElement('li');
+        li.textContent = hobby;
+        ul.appendChild(li);
+    }
+);
+
+div4.innerHTML += `<h4>Hobbies</h4>`;
+div4.appendChild(ul);
+
+section1.appendChild(div4);
