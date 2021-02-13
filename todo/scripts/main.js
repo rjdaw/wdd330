5f2beaaacb1544b8fba2cb51e4fb3635f621d87f
rjdaw <daw19003@byui.edu>
2021-02-13 12:06:52 -0700

    updating todo

--- todo/scripts/main.js
+++ todo/scripts/main.js
@@ -1,20 +1,57 @@
 let tasks = [];
 
-function addTask () {
-    if (isEmpty(document.querySelector(".add-text").value)) {
-        tasks.push(document.querySelector(".add-text").value);
-        display();
+window.onload = function() {
+  if (JSON.parse(localStorage.getItem("tasks")) != null)
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  display();
+};
+
+function addTask() {
+  if (!isEmpty(document.querySelector(".add-text").value)) {
+    tasks.push(document.querySelector(".add-text").value);
+    if (localStorage.getItem("tasks") == null) {
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+    } else {
+      localStorage.setItem("tasks", JSON.stringify(tasks));
     }
+    display();
+    document.querySelector(".add-text").value = "";
+  }
 }
 
 function isEmpty(str) {
-    return (!str || str.length === 0);
+  return (!str || str.length === 0);
 }
 
 function display() {
-    document.querySelector(".list").innerHTML = "";
+  document.querySelector(".list").innerHTML = "";
+  for (let i = 0; i < tasks.length; i++)
+    document.querySelector(".list").innerHTML +=
+      "<div class='task'>" +
+      tasks[i] +
+      "<img class='checkmark' src='images/checkmark.png' onclick='strike(" + i + ")'><img class='archive' src='images/archive.png' onclick='archive(" + i + ")'></div><br>";
+}
 
-    for (let i = 0; i <  tasks.length; i++) {
-        document.querySelector(".list").innerHTML += "<div class='task'>" + tasks[i] + "<img class='checkmark' src='images/checkmark.png' onclick='strike("+i+")'><img class='archive' src='images/delete-icon.jpg' onclick='archive("+i+")'></div>";
-    }
+function archive(index) {
+  tasks.splice(index, 1);
+  if (localStorage.getItem("tasks") == null) {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  } else {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }
+  display();
+}
+
+function strike(index) {
+  if (tasks[index].includes("<strike>")) {
+    tasks[index] = tasks[index].replace("<strike>", "");
+  } else {
+    tasks[index] = "<strike>" + tasks[index] + "</strike>";
+  }
+  if (localStorage.getItem("tasks") == null) {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  } else {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }
+  display();
 }
