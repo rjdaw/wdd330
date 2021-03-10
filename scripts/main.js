232051837c5b0074945a54ccef61e784fa80486e
rjdaw <daw19003@byui.edu>
2021-03-09 20:33:05 -0700

    notes week10

--- scripts/main.js
+++ scripts/main.js
@@ -36,6 +36,10 @@ const links = [
     {
         label: "Week 9 Notes",
         url: "../week9/index.html"
+    },
+    {
+        label: "Week 10 Notes",
+        url: "../week10/index.html"
     }
 ];
 
